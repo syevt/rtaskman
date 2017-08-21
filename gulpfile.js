53c807549b5961e8d6fc8fd1ef41851afe72fa10
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-08-21 17:49:20 +0300

    Add gulpfile  and have it run for the first time

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  open = require('gulp-open'),
+  browserify = require('browserify'),
+  coffeeify = require('coffeeify'),
+  watch = require('gulp-watch'),
+  plumber = require('gulp-plumber'),
+  tap = require('gulp-tap'),
+  streamify = require('gulp-streamify'),
+  concat = require('gulp-concat'),
+  stylus = require('gulp-stylus'),
+  pug = require('gulp-pug');
+
+gulp.task('bundle', function(){
+  gulp.src('./src/client/application.coffee', {read: false})
+    .pipe(plumber())
+    .pipe(tap(function(file){
+      var d = require('domain').create();
+      d.on('error', function(err){
+        gutil.log(gutil.colors.red("Browserify compile error:"),
+          gutil.colors.yellow(err.message),
+          gutil.colors.red("in line"),
+          gutil.colors.yellow(err.line),
+          gutil.colors.red("column"),
+          gutil.colors.yellow(err.column));
+        gutil.beep();
+        this.emit('end');
+      });
+      d.run(function(){
+        file.contents = browserify({
+          entries: [file.path],
+          extensions: ['.coffee'],
+          debug: true
+        }).transform(coffeeify).bundle();
+      });
+    }))
+    .pipe(streamify(concat('bundle.js')))
+    .pipe(gulp.dest('./public/js'))
+});
+
+gulp.task('pug', function(){
+  gulp.src('./src/client/**/**/*.jade')
+    .pipe(pug())
+    .pipe(gulp.dest('./public'))
+});
+
+gulp.task('stylus', function(){
+  gulp.src('./src/client/stylus/style.styl')
+    .pipe(stylus({
+      'include css': true
+    }))
+    .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./src/client/**/*.coffee', ['bundle']);
+  gulp.watch('./src/client/**/**/*.jade', ['jade']);
+  gulp.watch('./src/client/stylus/**/*.styl', ['stylus']);
+});
+
+gulp.task('default', ['bundle', 'pug', 'stylus', 'watch'])
