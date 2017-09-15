406b871e09c25549134622c842bf5665dc646c46
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2017-09-15 17:31:44 +0300

    Add project tasks json schema validation

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'activerecord_json_validator'
 gem 'devise_token_auth'
 gem 'hamlit-rails'
 gem 'omniauth'
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,14 @@
+require_relative 'schemas/tasks'
+
 class Project < ApplicationRecord
   belongs_to :user
 
   validates :name,
             presence: true,
-            format: { with: /\A[\p{Alnum}\s]+\z/ }
+            format: { with: /\A[\p{Alnum}.\s]+\z/ }
+
+  validates :tasks, json: {
+    message: ->(errors) { errors },
+    schema: TASKS_SCHEMA
+  }
 end
