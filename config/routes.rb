1e6c03ff11b9ef112b17694a3882f62e4b283d62
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-04-27 16:55:30 +0300

    Fix routes to use shorter except instead of only

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :projects, only: [:index, :create, :update, :destroy],
+      resources :projects, except: [:new, :show, :edit],
                            defaults: { format: :json }
       resources :tasks, only: [:create, :update, :destroy]
     end
